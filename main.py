cbd3828d74ed5cb7105e7c9003c2c88e192e96cc
Tyler Margetts <tmargett@iu.edu>
2019-08-30 14:28:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print("My name is Tyler Margetts.")
+print("My favorite game is chess, but my favorite videogame is FIFA.")
+print("I am concerned about not being able to know how to code what I want to make in a game.")
+print("I am really excited for my electricity of the brain laboratory class.")
+print("My stackoverflow username is: 11990752.")
+print("The URL to my github profile is: https://github.com/tylermargetts.")
