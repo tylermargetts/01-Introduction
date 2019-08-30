d9ab1c83ea51bc1dc9b1976cbfb0aa3ef49e54eb
Tyler Margetts <tmargett@iu.edu>
2019-08-30 15:10:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import utils
+import time
 
 utils.check_version((3,7))
 utils.clear()
@@ -10,4 +11,19 @@ print("My favorite game is chess, but my favorite videogame is FIFA.")
 print("I am concerned about not being able to know how to code what I want to make in a game.")
 print("I am really excited for my electricity of the brain laboratory class.")
 print("My stackoverflow username is: 11990752.")
-print("The URL to my github profile is: https://github.com/tylermargetts.")
+print("The URL to my github profile is: https://github.com/tylermargetts")
+
+answer = input("Would you like to hear a joke? ")
+if answer.lower().strip() == 'yes':
+    print("Why did the old man fall in the well? ")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print("Because he couldn't see that well.")
+else:
+    print("Alright then, have a nice day!")
+
